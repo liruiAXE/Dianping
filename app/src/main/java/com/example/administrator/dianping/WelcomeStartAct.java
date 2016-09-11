dcaebe78a219f0f4fd36f0721110798a815fe6ca
lirui <1420692335@qq.com>
2016-09-11 16:50:10 +0800

    用Timer实现跳转

--- app/src/main/java/com/example/administrator/dianping/WelcomeStartAct.java
+++ app/src/main/java/com/example/administrator/dianping/WelcomeStartAct.java
@@ -8,19 +8,31 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WelcomeStartAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_start);
-        new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
-                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-                return true;
-            }
-        }).sendEmptyMessageDelayed(0,3000);
+//        new Handler(new Handler.Callback() {
+//            @Override
+//            public boolean handleMessage(Message msg) {
+//                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+//        }).sendEmptyMessageDelayed(0,3000);
+        Timer timer=new Timer();
+        timer.schedule(new Task(),3000);
+    }
+    class Task extends TimerTask{
+        @Override
+        public void run() {
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
